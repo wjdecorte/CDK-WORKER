d0b014af5820e61f0581658e7c73e5aa7f7d4c11
Jason DeCorte <jason@stitchstudio.ai>
2025-05-07 08:39:00 -0400

    feat: add text extract notification lambda and queue to subscribe to sns; move text extract notification code to separate method

---
+++ src/stitch_worker/handlers/text_extract_notification/index.py
@@ -0,0 +1,6 @@
+# TODO: Implement the text extraction notification handler
+
+
+def handler(event, context):
+    print(event)
+    return {"statusCode": 200, "body": "Text extraction notification received"}
--- src/stitch_worker/stitch_worker_stack.py
+++ src/stitch_worker/stitch_worker_stack.py
@@ -7,6 +7,7 @@ from aws_cdk import (
     aws_events_targets,
     aws_iam,
     aws_ecr,
+    aws_sns_subscriptions,
     Duration,
     Tags,
     aws_sns,
@@ -24,31 +25,25 @@ class StitchWorkerStack(Stack):
         tags = self.node.try_get_context("tags")
         naming = self.node.try_get_context("naming")
         settings = self.node.try_get_context("settings")
-        prefix = naming["prefix"]
-        suffix = naming["suffix"]
+        self.prefix = naming["prefix"]
+        self.suffix = naming["suffix"]
 
         # Apply tags to all resources in the stack
         for key, value in tags.items():
             Tags.of(self).add(key, value)
 
-        # Create EventBridge Bus
-        bus = aws_events.EventBus(self, "StitchEventBridgeBus", event_bus_name=f"{prefix}-{suffix}-datastores-bus")
-
-        # Create SNS Topic
-        topic = aws_sns.Topic(
-            self, "TextExtractionTopic", topic_name=f"AmazonTextract-{prefix}-{suffix}-text-extraction-topic"
+        repository = aws_ecr.Repository.from_repository_arn(
+            self,
+            "StitchWorkerRepository",
+            repository_arn="arn:aws:ecr:us-east-2:613563724766:repository/stitch-worker",
         )
 
-        # Create IAM Role for Textract to publish to SNS Topic
-        role = aws_iam.Role(
-            self,
-            "TextractSNSRole",
-            role_name=f"{prefix}-{suffix}-textract-sns-role",
-            assumed_by=aws_iam.ServicePrincipal("textract.amazonaws.com"),
-            managed_policies=[
-                aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonTextractServiceRole")
-            ],
-            description="Role for Textract to publish to SNS Topic",
+        image_tag = settings["lambda_image_tag"]
+        print(f"Using image tag: {image_tag}")
+
+        # Create EventBridge Bus
+        bus = aws_events.EventBus(
+            self, "StitchEventBridgeBus", event_bus_name=f"{self.prefix}-{self.suffix}-datastores-bus"
         )
 
         # Define process names
@@ -111,22 +106,14 @@ class StitchWorkerStack(Stack):
             },
         ]
 
-        repository = aws_ecr.Repository.from_repository_arn(
-            self,
-            "StitchWorkerRepository",
-            repository_arn="arn:aws:ecr:us-east-2:613563724766:repository/stitch-worker",
-        )
-
-        image_tag = settings["lambda_image_tag"]
-        print(f"Using image tag: {image_tag}")
-
+        topic, queue, role = self.create_text_extraction_notification_lambda()
         # Create SQS queues and Lambda functions for each process
         for process in processes:
             # Create SQS queue
             queue = aws_sqs.Queue(
                 self,
                 f"{process['id_prefix']}Queue",
-                queue_name=f"{prefix}-{suffix}-{process['name']}",
+                queue_name=f"{self.prefix}-{self.suffix}-{process['name']}",
                 visibility_timeout=Duration.seconds(300),
                 retention_period=Duration.days(14),
             )
@@ -135,7 +122,7 @@ class StitchWorkerStack(Stack):
             lambda_fn = aws_lambda.DockerImageFunction(
                 self,
                 f"{process['id_prefix']}Lambda",
-                function_name=f"{prefix}-{suffix}-{process['name']}",
+                function_name=f"{self.prefix}-{self.suffix}-{process['name']}",
                 code=aws_lambda.DockerImageCode.from_ecr(
                     repository=repository,
                     tag_or_digest=image_tag,
@@ -185,22 +172,23 @@ class StitchWorkerStack(Stack):
             lambda_fn.add_event_source(aws_lambda_event_sources.SqsEventSource(queue))
 
             # Create EventBridge rule
-            aws_events.Rule(
-                self,
-                id=f"Stitch{process['id_prefix']}EventRule",
-                enabled=True,
-                event_bus=bus,
-                rule_name=f"{prefix}-{suffix}-{process['name']}",
-                event_pattern=aws_events.EventPattern(**process["event_pattern"]),
-                targets=[aws_events_targets.SqsQueue(queue)],
-            )
+            if process["event_pattern"]:
+                aws_events.Rule(
+                    self,
+                    id=f"Stitch{process['id_prefix']}EventRule",
+                    enabled=True,
+                    event_bus=bus,
+                    rule_name=f"{self.prefix}-{self.suffix}-{process['name']}",
+                    event_pattern=aws_events.EventPattern(**process["event_pattern"]),
+                    targets=[aws_events_targets.SqsQueue(queue)],
+                )
 
         # Create EventBridge rule for S3 Object Created on default event bus
         aws_events.Rule(
             self,
             "StitchDocumentUploadEventRule",
             enabled=True,
-            rule_name=f"{prefix}-{suffix}-document-upload",
+            rule_name=f"{self.prefix}-{self.suffix}-document-upload",
             event_pattern=aws_events.EventPattern(
                 source=["aws.s3"],
                 detail_type=[EventType.S3_OBJECT_CREATED],
@@ -211,6 +199,51 @@ class StitchWorkerStack(Stack):
             ),
             targets=[aws_events_targets.EventBus(bus)],
         )
+
+    def create_text_extraction_notification_lambda(self):
+        # Create SNS Topic
+        topic = aws_sns.Topic(
+            self, "TextExtractionTopic", topic_name=f"AmazonTextract-{self.prefix}-{self.suffix}-text-extraction-topic"
+        )
+
+        # Create SQS Queue for SNS Topic
+        queue = aws_sqs.Queue(
+            self,
+            "TextExtractionNotificationQueue",
+            queue_name=f"{self.prefix}-{self.suffix}-text-extraction-notification",
+        )
+
+        # Add SNS Topic to SQS Queue
+        topic.add_subscription(aws_sns_subscriptions.SqsSubscription(queue))
+
+        # Add Lambda Function to SQS Queue
+        lambda_fn = aws_lambda.Function(
+            self,
+            "TextExtractionNotificationLambda",
+            function_name=f"{self.prefix}-{self.suffix}-text-extraction-notification",
+            code=aws_lambda.Code.from_asset("src/stitch_worker/handlers/text_extract_notification"),
+            handler="index.handler",
+            runtime=aws_lambda.Runtime.PYTHON_3_12,
+            environment={
+                "TEXT_EXTRACTION_SNS_TOPIC_ARN": topic.topic_arn,
+            },
+        )
+
+        lambda_fn.add_event_source(aws_lambda_event_sources.SqsEventSource(queue))
+
+        # Create IAM Role for Textract to publish to SNS Topic
+        role = aws_iam.Role(
+            self,
+            "TextractSNSRole",
+            role_name=f"{self.prefix}-{self.suffix}-textract-sns-role",
+            assumed_by=aws_iam.ServicePrincipal("textract.amazonaws.com"),
+            managed_policies=[
+                aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonTextractServiceRole")
+            ],
+            description="Role for Textract to publish to SNS Topic",
+        )
+
+        return topic, queue, role
 
 
 class StitchOrchestrationStack(Stack):
