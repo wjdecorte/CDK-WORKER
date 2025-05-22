08e7a381e18d840876374f2926f968be763e5ef1
Jason DeCorte <jason@stitchstudio.ai>
2025-05-22 14:15:49 -0400

    feat: setting priority for dotenv file over env vars

--- src/stitch_worker/__init__.py
+++ src/stitch_worker/__init__.py
@@ -1,7 +1,24 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource
 
 
 class StitchWorkerSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
     lambda_image_tag: str
+    lambda_block_standardization: bool = True
+    lambda_document_summary: bool = False
+    lambda_seed_questions: bool = False
+    lambda_feature_extraction: bool = False
+    lambda_document_extraction: bool = True
+    lambda_split_file: bool = False
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return dotenv_settings, env_settings, init_settings
