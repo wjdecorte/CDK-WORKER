7f81a4d49690a300ba33697923460403d9f6f0a8
Jason DeCorte <jason@stitchstudio.ai>
2025-05-22 07:56:05 -0400

    feat: add new enums

--- src/stitch_worker/enums.py
+++ src/stitch_worker/enums.py
@@ -2,11 +2,12 @@ from enum import StrEnum
 
 
 class EventType(StrEnum):
-    BLOCK_PROCESSING_COMPLETED = "BlockProcessingCompleted"
+    BLOCK_STANDARDIZATION_COMPLETED = "BlockStandardizationCompleted"
     DOCUMENT_SUMMARY_GENERATED = "DocumentSummaryGenerated"
     SEED_QUESTIONS_GENERATED = "SeedQuestionsGenerated"
     FEATURE_EXTRACTION_COMPLETED = "FeatureExtractionCompleted"
     DOCUMENT_EXTRACTION_COMPLETED = "DocumentExtractionCompleted"
+    DOCUMENT_EXTRACTION_STARTED = "DocumentExtractionStarted"
     S3_OBJECT_CREATED = "Object Created"
     SPLIT_FILE_COMPLETED = "SplitFileCompleted"
     TEXT_EXTRACT_SYNC_COMPLETED = "TextExtractSyncCompleted"
